0882dcbc2ecf978ce046d9cdfa6669ac9ada6a1e
Jonny Miyagi <liltrees98@gmail.com>
2021-07-26 19:01:34 -0700

    Initial Commit

---
+++ EcoShipShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoShipShare
+//
+//  Created by JŌNNY MĪYĀGĪ on 7/26/21.
+//  Copyright © 2021 JŌNNY MĪYĀGĪ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
